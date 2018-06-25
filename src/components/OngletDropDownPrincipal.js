275937861b207a2264c56fa2b6e1bdcc02612b0b
Ilmio <facchin.carlos@yahoo.fr>
2018-06-25 13:25:37 +0200

    ajout du component onglet drop down principal

---
+++ src/components/OngletDropDownPrincipal.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import './OngletDropDownPrincipal.css';
+
+class OngletDropDownPrincipal extends Component {
+  render() {
+    return(
+      <a className="dropdown-toggle" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        ECHANGER
+      </a>
+    )
+  }
+}
+
+export default OngletDropDownPrincipal
