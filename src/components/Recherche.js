2f84692527eac9346d97be3ddbbfd7b72c32a2b0
Ilmio <facchin.carlos@yahoo.fr>
2018-06-25 13:34:11 +0200

    ajout du component recherche

---
+++ src/components/Recherche.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+import './Recherche.css';
+
+class Recherche extends Component {
+  render() {
+    return(
+      <p>Rechercher sur le site</p>
+    )
+  }
+}
+
+export default Recherche
