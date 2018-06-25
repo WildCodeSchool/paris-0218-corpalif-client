582163510650edcd29e2b2b8e9aa8cdf4e517b7a
Ilmio <facchin.carlos@yahoo.fr>
2018-06-25 13:24:11 +0200

    ajout du component onglet

---
+++ src/components/Onglet.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+import './Onglet.css';
+
+class Onglet extends Component {
+  render() {
+    return(
+      <li><a href="#Echanger">ECHANGER</a></li>
+    )
+  }
+}
+
+export default Onglet
