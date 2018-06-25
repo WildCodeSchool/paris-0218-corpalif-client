b7c6e2baadd64bf82b9d1241e662938e7c2a10f4
Ilmio <facchin.carlos@yahoo.fr>
2018-06-25 13:33:42 +0200

    ajout du component onglet rouge

---
+++ src/components/OngletRouge.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './OngletRouge.css';
+
+class OngletRouge extends Component {
+  render() {
+    return(
+      <li className="OngletRouge"><a href="#FormulaireAdmission">FORMULAIRE D'ADMISSION EN USP</a></li>
+    )
+  }
+}
+
+export default OngletRouge
