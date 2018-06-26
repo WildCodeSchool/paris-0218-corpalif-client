4c69a1de2032aa9542dc8f34fbfe39904d3403bd
Ilmio <facchin.carlos@yahoo.fr>
2018-06-26 11:08:51 +0200

    test des stories

--- src/stories/index.js
+++ src/stories/index.js
@@ -2,17 +2,12 @@ import React from 'react';
 
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
-import { linkTo } from '@storybook/addon-links';
 
-import { Button, Welcome } from '@storybook/react/demo';
-
-storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
+import { Button } from '@storybook/react/demo';
+import RedButton from '../components/RedButton.js';
+import Onglet from '../components/Onglet.js';
 
 storiesOf('Button', module)
-  .add('with text', () => 
-    <Button onClick={action('clicked')}>
-      Hello Button
-    </Button>)
   .add('with some emoji', () => 
     <Button onClick={action('clicked')}>
       <span role="img" aria-label="emojie" >😀 😎 👍 💯</span>
@@ -21,3 +16,19 @@ storiesOf('Button', module)
     <Button onClick={action('clicked')} style={{color: 'red'}}>
       Teste mon BOUTON ROUGE !
     </Button>);
+
+storiesOf('RedButton', module)
+  .add('with text', () => 
+    <RedButton onClick={action('clicked')}>
+      Hello Button
+    </RedButton>)
+    .add('with text, black fond', () => 
+    <RedButton onClick={action('clicked')} style={{background: 'black'}} >
+      Bienvenue
+    </RedButton>);
+
+storiesOf('Onglet', module)
+.add('with text', () => 
+  <Onglet onClick={action('clicked')}>
+    Hello Button
+  </Onglet>);
