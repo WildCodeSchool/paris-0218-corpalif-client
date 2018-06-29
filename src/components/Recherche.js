bcf73d66559b6357db880330b0578e15eab5b4e3
Ilmio <facchin.carlos@yahoo.fr>
2018-06-29 11:53:51 +0200

    mise en place du component recherche

--- src/components/Recherche.js
+++ src/components/Recherche.js
@@ -3,7 +3,14 @@ import React from 'react';
 import './Recherche.css';
 
 const Recherche = () =>
-      <p>Rechercher sur le site</p>
+<div className="row">
+  <div className="input-group">
+    <input type="text" className="form-control" placeholder="Rechercher sur le site" />
+    <span className="input-group-btn">
+      <button className="btn btn-default" type="button"><span role="img" aria-label="loupe">🔍</span></button>
+    </span>
+  </div>
+</div>
 
 
 export default Recherche
