848026508593283150b2f71fd259626528d8d68e
Ilmio <facchin.carlos@yahoo.fr>
2018-06-29 11:52:10 +0200

    modification de l'image home pour avoir un logo home blanc

--- src/components/OngletImg.js
+++ src/components/OngletImg.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import './OngletImg.css';
 
-import Home from '../img/Home.png';
+import HomeWhite from '../img/HomeWhite.png';
 
 const OngletImg = () => 
-  <li><a href="#Home"><img className="LittleLogo" src={Home} alt="Facebook"></img></a></li>
+  <li><a href="#Home"><img className="LittleLogo" src={HomeWhite} alt="Facebook"></img></a></li>
 
 
 export default OngletImg
