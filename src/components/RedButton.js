3da7c72afa3e7f46090fd32c380137ddd73f1ea8
Ilmio <facchin.carlos@yahoo.fr>
2018-07-01 11:52:34 +0200

    ajout de styled-components, suppression du fichier css et ajout du eslint disable / enable du component RedButton

--- src/components/RedButton.js
+++ src/components/RedButton.js
@@ -1,11 +1,37 @@
 import React from 'react';
 
-import "./RedButton.css";
+import Styled from 'styled-components';
 
+
+// JSX
 const RedButton = ({ children, ...rest }) => 
-  <button className="RedButton" {...rest}>
-    {children}
-  </button>
+    <Button {...rest}>
+      {children}
+    </Button>
+
+
+// Styled-components
+// eslint-disable 
+const Button = Styled.button`
+  height: 30px;
+  padding: 0 15px;
+  color: white;
+  font-weight: bold;
+  background-color: rgb(204, 73, 73);
+  border: none;
+
+  transition: color 0.3s;
+  transition: background-color 0.3s;
+
+  ${Button}:hover {
+    background-color: rgb(142, 32, 33);
+    color: rgb(201, 201, 201);
+
+    transition: color 0.4s;
+    transition: background-color 0.4s;
+  }
+`;
+// eslint-enable
 
 
 export default RedButton
