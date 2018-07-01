367511d62feae6c0c7bb2b364dcf656f1cb94ac6
Ilmio <facchin.carlos@yahoo.fr>
2018-07-01 11:53:31 +0200

    ajout de styled-components, suppression du fichier css et ajout du eslint disable / enable du component Onglet

--- src/components/Onglet.js
+++ src/components/Onglet.js
@@ -1,13 +1,44 @@
 import React from 'react';
 
-import './Onglet.css';
+import Styled from 'styled-components';
 
+
+// JSX
 const Onglet = ({ children }) =>
-  <li className="Onglet">
-    <a>
+  <Li>
+    <A href="#Onglet">
       {children}
-    </a>
-  </li>
+    </A>
+  </Li>
+
+
+// Styled-components
+// eslint-disable 
+const Li = Styled.li`   
+  ${Li}:hover {
+    color: rgb(51, 116, 103);
+    transition: color 0.4s;
+}
+`;
+
+const A = Styled.a`
+  display: flex;
+  align-items: center;
+  height: 50px;
+  padding: 0 1em;
+
+  font-weight: bold;
+  color: white;
+
+  transition: color 0.3s;
+
+  ${A}:hover {
+    text-decoration:none; 
+    color: rgb(51, 116, 103);
+    transition: color 0.4s;
+  }
+`;
+// eslint-enable
 
 
 export default Onglet
