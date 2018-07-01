b6927bbc3219429ed748c607681763462928972b
Ilmio <facchin.carlos@yahoo.fr>
2018-07-01 11:47:32 +0200

    ajout de styled-components, suppression du fichier css et ajout du eslint disable / enable du component LittleLogo

--- src/components/LittleLogo.js
+++ src/components/LittleLogo.js
@@ -1,12 +1,33 @@
 import React from 'react';
 
-import './LittleLogo.css';
+import Styled from 'styled-components';
 
 import Logo_Facebook from "../img/logo_facebook.png";
 
 
+// JSX
 const LittleLogo = () =>
-  <a href="https://fr-fr.facebook.com/Corpalif/"><img className="LittleLogo" src={Logo_Facebook} alt="Facebook"></img></a>
+  <a href="https://fr-fr.facebook.com/Corpalif/">
+    <Img src={Logo_Facebook} alt="Facebook" />
+  </a>
+
+
+// Styled-components
+// eslint-disable 
+const Img = Styled.img`
+  height: 40px;
+  width: 40px;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  ${Img}:hover {
+    height: 40px;
+    width: 40px;
+  }
+`;
+// eslint-enable
 
 
 export default LittleLogo
