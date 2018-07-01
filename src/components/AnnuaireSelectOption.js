b4b21b27f82b61cdbeb1d0ca2f730871eb44b9b2
Ilmio <facchin.carlos@yahoo.fr>
2018-07-01 16:59:52 +0200

    creation du component AnnuaireSelectOption

---
+++ src/components/AnnuaireSelectOption.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import Styled from 'styled-components';
+
+
+// JSX
+const AnnuaireSelectOption = ({ children }) =>
+    <Option>{children}</Option>
+
+
+// Styled-components
+// eslint-disable 
+const Option = Styled.option`
+
+`;
+// eslint-enable
+
+
+export default AnnuaireSelectOption
