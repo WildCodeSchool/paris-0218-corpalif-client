3791185e3ba6400604d7a70774a39b996a730e64
Ilmio <facchin.carlos@yahoo.fr>
2018-07-01 11:50:53 +0200

    ajout de styled-components, suppression du fichier css et ajout du eslint disable / enable du component OngletImg

--- src/components/OngletImg.js
+++ src/components/OngletImg.js
@@ -1,10 +1,53 @@
 import React from 'react';
-import './OngletImg.css';
+
+import Styled from 'styled-components';
 
 import HomeWhite from '../img/HomeWhite.png';
+// import HomeBlack from '../img/HomeBlack.png';
 
-const OngletImg = () => 
-  <li><a href="#Home"><img className="LittleLogo" src={HomeWhite} alt="Facebook"></img></a></li>
+
+// JSX
+  const OngletImg = () => 
+  <li>
+    <A href="#Home">
+      <Img src={HomeWhite} ></Img>  {/*onMouseOver={e => (e.currentTarget.src={HomeBlack} )} */}
+    </A>
+  </li>
+
+
+// Styled-components
+// eslint-disable 
+const Img = Styled.img`
+  height: 40px;
+  width: 40px;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  ${Img}:hover {
+    height: 40px;
+    width: 40px;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+`;
+
+const A = Styled.a`
+  display: flex;
+  align-items: center;
+  height: 50px;
+  padding: 0 1em;
+  ${A}:hover {
+    display: flex;
+    align-items: center;
+    height: 50px;
+    padding: 0 1em;
+  }
+`;
+// eslint-enable
 
 
 export default OngletImg
