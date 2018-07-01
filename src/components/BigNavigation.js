cc492dca747375f57b7935df2952d806781aaae6
Ilmio <facchin.carlos@yahoo.fr>
2018-07-01 11:46:04 +0200

    ajout de styled-components, suppression du fichier css et ajout du eslint disable / enable du component BigNavigation

--- src/components/BigNavigation.js
+++ src/components/BigNavigation.js
@@ -1,24 +1,43 @@
 import React from 'react';
-import './BigNavigation.css';
+
+import Styled from 'styled-components';
 
 import Onglet from './Onglet.js';
 import OngletRouge from './OngletRouge.js';
-import OngletDropDown from './OngletDropDown.js';
+import OngletDropDown01 from './OngletDropDown01';
+import OngletDropDown02 from './OngletDropDown02';
+import OngletDropDown04 from './OngletDropDown04';
+import OngletDropDown05 from './OngletDropDown05';
 import OngletImg from './OngletImg.js';
 
+
+// JSX
 const BigNavigation = () =>
-  <nav className="">
-    <ul className="BigNavigation">
+  <nav>
+    <Ul className="BigNavigation">
       <OngletImg />
-      <OngletDropDown />
+      <OngletDropDown01 />
       
-      <Onglet children="SOINS PALLIATIFS" href={'soins-palliatifs'}/>
+      <OngletDropDown02 />
       <Onglet children="ANNUAIRE FRANCILIEN" href={'annuaire-francilien'}/>
-      <Onglet children="EMPLOI / FORMATION" href={'emploi-formation'}/>
-      <Onglet children="VEILLE MÉDICALE" href={'veille-medicale'}/>
+      <OngletDropDown04 />
+      <OngletDropDown05 />
       <OngletRouge children="FORMULAIRE D'ADMISSION EN USP" href={'formulaire-d-admission-en-usp'}/>
-    </ul>
+    </Ul>
   </nav>
+
+
+// Styled-components
+// eslint-disable 
+const Ul = Styled.ul`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  height: 50px;
+  background-color: rgb(16, 73, 73);
+`;
+// eslint-enable
 
 
 export default BigNavigation
