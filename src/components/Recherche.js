c3d758d7be36490d16b01fe3fe64aac5e19ee83b
Ilmio <facchin.carlos@yahoo.fr>
2018-07-01 11:52:11 +0200

    ajout de styled-components, suppression du fichier css et ajout du eslint disable / enable du component Recherche

--- src/components/Recherche.js
+++ src/components/Recherche.js
@@ -1,16 +1,50 @@
 import React from 'react';
 
-import './Recherche.css';
+import Styled from 'styled-components';
 
+
+// JSX
 const Recherche = () =>
 <div className="row">
   <div className="input-group">
-    <input type="text" className="form-control" placeholder="Rechercher sur le site" />
+    <Input type="text" className="form-control" placeholder="Rechercher sur le site" />
     <span className="input-group-btn">
-      <button className="btn btn-default" type="button"><span role="img" aria-label="loupe">🔍</span></button>
+      <Button className="btn btn-default" type="button"><span role="img" aria-label="loupe">🔍</span></Button>
     </span>
   </div>
 </div>
+
+
+// Styled-components
+// eslint-disable 
+const Button = Styled.button`
+  height: 30px;
+  font-size: 1.2em;
+  color: white;
+  background-color: rgb(204, 73, 73);
+  border-radius: 0;
+  border: none;
+  padding: 0 5px;
+
+  transition: color 0.3s;
+  transition: background-color 0.3s;
+
+
+  ${Button}:hover {
+    background-color: rgb(142, 32, 33);
+    color: rgb(201, 201, 201);
+
+    transition: color 0.4s;
+    transition: background-color 0.4s;
+  }
+`;
+
+const Input = Styled.input`
+  height: 30px;
+  font-size: 0.8em;
+  border-radius: 0;
+`;
+// eslint-enable
 
 
 export default Recherche
