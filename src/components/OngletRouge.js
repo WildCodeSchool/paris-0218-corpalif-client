3a3546055c455fbadbf51703fcdc3e9f74a1bd4e
Ilmio <facchin.carlos@yahoo.fr>
2018-07-01 11:51:37 +0200

    ajout de styled-components, suppression du fichier css et ajout du eslint disable / enable du component OngletRouge

--- src/components/OngletRouge.js
+++ src/components/OngletRouge.js
@@ -1,12 +1,46 @@
 import React from 'react';
-import './OngletRouge.css';
 
+import Styled from 'styled-components';
+
+
+// JSX
 const OngletRouge = ({ children }) =>
-  <li className="OngletRouge">
-    <a href="#FormulaireAdmission">
+  <Li>
+    <A href="#Ongletrouge">
       {children}
-    </a>
-  </li>
+    </A>
+  </Li>
+
+
+// Styled-components
+// eslint-disable 
+const Li = Styled.li`
+  background-color: rgb(204, 73, 73);
+  transition: background-color 0.3s;
+
+
+  ${Li}:hover {
+    background-color: rgb(142, 32, 33);
+    transition: background-color 0.4s;
+  }
+`;
+
+const A = Styled.a`
+  display: flex;
+  align-items: center;
+  height: 50px;
+  padding: 0 1em;
+  font-weight: bold;
+  color: white;
+  text-decoration: none;
+
+ ${A}:hover {
+  text-decoration:none; 
+  color: rgb(201, 201, 201);
+  transition: color 0.4s;
+  }
+`;
+// eslint-enable
 
 
 export default OngletRouge
