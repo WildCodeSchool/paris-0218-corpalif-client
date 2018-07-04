f1394fc348d556e583ca82af2714d098cd6e29b6
Ilmio <facchin.carlos@yahoo.fr>
2018-07-04 18:52:28 +0200

    finalisation du component Formulaire

--- src/components/Formulaire.js
+++ src/components/Formulaire.js
@@ -18,118 +18,119 @@ const Formulaire = () =>
           <DivCentrer>
             <DivFloat>
               <Form>
-                <div className="row">
-                  <div className="col-2">
-                    <label for="inputCivilite">Civilité</label>
-                  </div>
-                  <div className="col-4">
-                    <select id="inputState" className="form-control">
+                <DivRow className="row justify-content-center">
+                  <DivLabel className="col-3">
+                    <Label for="inputCivilite">Civilité</Label>
+                  </DivLabel>
+                  <div className="col-5">
+                    <Select id="inputState" className="form-control">
                       <option selected>Docteur</option>
                       <option>Professeur</option>
                       <option>Mademoiselle</option>
                       <option>Madame</option>
                       <option>Monsieur</option>
-                    </select>
+                    </Select>
                   </div>
+                  </DivRow>
+                <DivRow className="row justify-content-center">
+                  <DivLabel className="col-3">
+                    <Label for="inputNom">Nom</Label>
+                  </DivLabel>
+                  <div className="col-5">
+                    <Input type="text" className="form-control" />
                   </div>
-                <div className="row">
-                  <div className="col-2">
-                    <label for="inputNom">Nom</label>
+                </DivRow>
+                <DivRow className="row justify-content-center">
+                  <DivLabel className="col-3">
+                    <Label for="inputPrenom">Prénom</Label>
+                  </DivLabel>
+                  <div className="col-5">
+                    <Input type="text" className="form-control" />
                   </div>
-                  <div className="col-4">
-                    <input type="text" className="form-control" placeholder="Nom" />
+                </DivRow>
+                <DivRow className="row justify-content-center">
+                  <DivLabel className="col-3">
+                    <Label for="inputEmail">Email</Label>
+                  </DivLabel>
+                  <div className="col-5">
+                    <Input type="email" className="form-control" id="inputEmail" />
                   </div>
-                </div>
-                <div className="row">
-                  <div className="col-2">
-                    <label for="inputPrenom">Prénom</label>
+                </DivRow>
+                <DivRow className="row justify-content-center">
+                  <DivLabel className="col-3">
+                    <Label for="inputPassword">Password</Label>
+                  </DivLabel>
+                  <div className="col-5">
+                    <Input type="password" className="form-control" id="inputPassword" />
                   </div>
-                  <div className="col-4">
-                    <input type="text" className="form-control" placeholder="Prénom" />
+                </DivRow>
+                <DivRow className="row justify-content-center">
+                  <DivLabel className="col-3">
+                    <Label for="inputAdresse">Adresse</Label>
+                  </DivLabel>
+                  <div className="col-5">
+                    <Input type="text" className="form-control" id="inputAdresse" />
                   </div>
-                </div>
-                <div className="row">
-                  <div className="col-2">
-                    <label for="inputEmail">Email</label>
+                </DivRow>
+                <DivRow className="row justify-content-center">
+                  <DivLabel className="col-3">
+                    <Label for="inputVille">Ville</Label>
+                  </DivLabel>
+                  <div className="col-5">
+                    <Input type="text" className="form-control" id="inputVille" />
                   </div>
-                  <div className="col-4">
-                    <input type="email" className="form-control" id="inputEmail" placeholder="Email" />
+                </DivRow>
+                <DivRow className="row justify-content-center">
+                  <DivLabel className="col-3">
+                    <Label for="inputCodePostale">Code Postale</Label>
+                  </DivLabel>
+                  <div className="col-5">
+                    <Input type="text" className="form-control" id="inputCodePostale" />
                   </div>
-                </div>
-                <div className="row">
-                  <div className="col-2">
-                    <label for="inputPassword">Password</label>
+                </DivRow>
+                <DivRow className="row justify-content-center">
+                  <DivLabel className="col-3">
+                    <Label for="inputTelephoneFixe">Téléphone Fixe</Label>
+                  </DivLabel>
+                  <div className="col-5">
+                    <Input type="text" className="form-control" id="inputTelephone Fixe" />
                   </div>
-                  <div className="col-4">
-                    <input type="password" className="form-control" id="inputPassword" placeholder="Password" />
+                </DivRow>
+                <DivRow className="row justify-content-center">
+                  <DivLabel className="col-3">
+                    <Label for="inputTelephoneMobile">Téléphone Mobile</Label>
+                  </DivLabel>
+                  <div className="col-5">
+                    <Input type="text" className="form-control" id="inputTéléphone Mobile" />
                   </div>
-                </div>
-                <div className="row">
-                  <div className="col-2">
-                    <label for="inputAdresse">Adresse</label>
+                </DivRow>
+                <DivRow className="row justify-content-center">
+                  <DivLabel className="col-3">
+                    <Label for="inputStructure">Structure</Label>
+                  </DivLabel>
+                  <div className="col-5">
+                    <Input type="text" className="form-control" id="inputStructure" />
                   </div>
-                  <div className="col-4">
-                    <input type="text" className="form-control" id="inputAdresse" placeholder="Adresse" />
+                </DivRow>
+                <DivRow className="row justify-content-center">
+                  <DivLabel className="col-3">
+                    <Label for="inputService">Service</Label>
+                  </DivLabel>
+                  <div className="col-5">
+                    <Input type="text" className="form-control" id="inputService" />
                   </div>
-                </div>
-                <div className="row">
-                  <div className="col-2">
-                    <label for="inputVille">Ville</label>
+                </DivRow>
+                <DivRow className="row justify-content-center">
+                  <DivLabel className="col-3">
+                    <Label for="inputFonction">Fonction</Label>
+                  </DivLabel>
+                  <div className="col-5">
+                    <Input type="text" className="form-control" id="inputFonction" />
                   </div>
-                  <div className="col-4">
-                    <input type="text" className="form-control" id="inputVille" placeholder="Ville" />
-                  </div>
-                </div>
-                <div className="row">
-                  <div className="col-2">
-                    <label for="inputCodePostale">Code Postale</label>
-                  </div>
-                  <div className="col-4">
-                    <input type="text" className="form-control" id="inputCodePostale" placeholder="Code Postale" />
-                  </div>
-                </div>
-                <div className="row">
-                  <div className="col-2">
-                    <label for="inputTelephoneFixe">Téléphone Fixe</label>
-                  </div>
-                  <div className="col-4">
-                    <input type="text" className="form-control" id="inputTelephone Fixe" placeholder="TéléphoneFixe" />
-                  </div>
-                </div>
-                <div className="row">
-                  <div className="col-2">
-                    <label for="inputTelephoneMobile">Téléphone Mobile</label>
-                  </div>
-                  <div className="col-4">
-                    <input type="text" className="form-control" id="inputTéléphone Mobile" placeholder="TelephoneMobile" />
-                  </div>
-                </div>
-                <div className="row">
-                  <div className="col-2">
-                    <label for="inputStructure">Structure</label>
-                  </div>
-                  <div className="col-4">
-                    <input type="text" className="form-control" id="inputStructure" placeholder="Structure" />
-                  </div>
-                </div>
-                <div className="row">
-                  <div className="col-2">
-                    <label for="inputService">Service</label>
-                  </div>
-                  <div className="col-4">
-                    <input type="text" className="form-control" id="inputService" placeholder="Service" />
-                  </div>
-                </div>
-                <div className="row">
-                  <div className="col-2">
-                    <label for="inputFonction">Fonction</label>
-                  </div>
-                  <div className="col-4">
-                    <input type="text" className="form-control" id="inputFonction" placeholder="Fonction" />
-                  </div>
-                </div>
-
-                <button type="submit" className="col-4">Valider</button>
+                </DivRow>
+                <DivRow className="row justify-content-center">
+                  <Button type="submit" className="col-2">Valider</Button>
+                </DivRow>
               </Form>
 
             </DivFloat>
@@ -141,34 +142,73 @@ const Formulaire = () =>
 
 // Styled-components
 // eslint-disable 
+const DivRow = Styled.div`
+  margin: 20px;
+`;
+
+const DivLabel = Styled.div`
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  height: 38px;
+  padding: 0 10px;
+  // border: 2px solid black;
+  background-color: rgb(16, 73, 73);
+`;
+
+const Label = Styled.label`
+  font-size: 18px;
+  font-weight: bold;
+`;
+
+const Input = Styled.input`
+height: 38px;
+  padding: 0 10px;
+  border: 1px solid lightgrey;
+  border-radius: 0;
+`;
+
+const Select = Styled.select`
+  height: 38px;
+  padding: 0 5px;
+  border: 1px solid lightgrey;
+  border-radius: 0;
+`;
+
+const Button = Styled.button`
+  height: 38px;
+  padding: 0 5px;
+  color: white;
+  font-size: 18px;
+  font-weight: bold;
+  background-color: rgb(16, 73, 73);
+`;
+
 const Form = Styled.form`
-width: 800px;
-  background: blue;
+  width: 800px;
+  background: white;
+  border: 5px solid rgb(16, 73, 73);
 `;
 
 const DivInformation = Styled.div`
   display: flex;
   flex-direction: column;
-
   width: 100%;
   padding: 50px;
-
   color: white;
-  background-color: rgb(16, 73, 73);
+  // background-color: rgb(16, 73, 73);
 `;
 
 const DivCentrer = Styled.div`
   display: flex;
   flex-direction: row;
   justify-content: center;
-
   margin: 15px;
 `;
 
 const DivFloat = Styled.div`
   display: flex;
   flex-direction: column;
-
   max-width: 1000px;
 `;
 // eslint-enable
